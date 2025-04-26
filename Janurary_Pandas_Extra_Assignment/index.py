950b4b74e1aed4d019824e66d9f870388bb92e57
Charles Norman <arioch3d@gmail.com>
2025-04-26 16:36:30 -0400

    Updated Index File

---
+++ Janurary_Pandas_Extra_Assignment/index.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Sample DataFrame
+df = pd.read_csv('rawdata/LMG_Inspection_Violations_of_Failed_Restaurants.csv')
+
+# Group by 'Premise Zip Code'
+filtered_df = df[df['score'] < 85]
+grouped = filtered_df.groupby('premise_zip')
+
+# Iterate through groups
+#for name, group in grouped:
+#    print(f"Group: {name}")
+    # Iterate through rows in each group
+#    for index, row in group.iterrows():
+        
+#        print(f"Zip Code: {row['premise_zip']}, Score: {row['score']}, Critical Violation: {row['critical_yn']}")
+effect = filtered_df["premise_zip"].value_counts()
+#filtered_df.plot(kind="bar", x="premise_zip", y="score", title="Inspections by Zip Code")
+#plt.plot()
+plt.bar(x=df['premise_zip'], height=df['score'], color=["red"])
+#plt.figure(figsize=(8,6))
+plt.ylabel("Number of Failed Inspections")
+plt.xlabel("Zip Code")
+plt.title("Restaurant Failed Inspections")
+plt.show()
