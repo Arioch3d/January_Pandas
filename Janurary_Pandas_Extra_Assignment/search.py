0362824ec82e8d8fd52f2b8a8fd85de6dd9d38f9
Charles Norman <arioch3d@gmail.com>
2025-05-06 12:01:58 -0400

    Another commit but forgot what I did

--- Janurary_Pandas_Extra_Assignment/search.py
+++ Janurary_Pandas_Extra_Assignment/search.py
@@ -7,7 +7,7 @@ from IPython.display import display
 data_df = pd.read_csv('rawdata/LMG_Inspection_Violations_of_Failed_Restaurants.csv')
 
 # Create textbox widget for numeric input
-textbox = widgets.IntText(description="Enter Value:")
+textbox = widgets.IntText(description="Enter Zip Code:")
 
 # Create output widget for displaying results
 output = widgets.Output()
@@ -23,10 +23,18 @@ def search_and_plot(value):
 
             if not filtered_df.empty:
                 # Create bar plot
-                plt.bar(filtered_df.index, filtered_df['score'])
+                plt.bar(filtered_df.index, filtered_df['score'], color=["red"])
                 plt.xlabel(f"Zip Code {value} \n {zipcode_count} \n  Failed Inspections Found")
                 plt.ylabel('Score')
-                plt.title(f'You search for Zip Code {value}')
+                plt.title(f'You searched for Zip Code {value}')
+                plt.axhline(y=10, xmin=0, xmax=100, color='b', linestyle='-')
+                plt.axhline(y=20, xmin=0, xmax=100, color='b', linestyle='-')
+                plt.axhline(y=30, xmin=0, xmax=100, color='b', linestyle='-')
+                plt.axhline(y=40, xmin=0, xmax=100, color='b', linestyle='-')
+                plt.axhline(y=50, xmin=0, xmax=100, color='b', linestyle='-')
+                plt.axhline(y=60, xmin=0, xmax=100, color='b', linestyle='-')
+                plt.axhline(y=70, xmin=0, xmax=100, color='b', linestyle='-')
+                plt.axhline(y=80, xmin=0, xmax=100, color='b', linestyle='-')
                 plt.show()
             else:
                  print(f"No matching Zip Codes found for {value}.")
@@ -34,11 +42,10 @@ def search_and_plot(value):
 # Observe changes in textbox value
 def on_value_change(change):
     search_and_plot(change['new'])
+    
 
 textbox.observe(on_value_change, names='value')
 
-# Display widgets
-display(textbox, output)
 
 # Group by 'Premise Zip Code'
 filtered_df = data_df[data_df['score'] < 85]
@@ -46,17 +53,20 @@ grouped = filtered_df.groupby('premise_zip')
 zipcode_count = filtered_df["premise_zip"].count()
 
 
+def plot():
+    plt.bar(x=filtered_df['premise_zip'], height=filtered_df['score'], color=["red"])
+    plt.ylabel("Scores")
+    plt.xlabel(f"Zip Code \n {zipcode_count} \n  Zip Codes Found")
+    plt.title("Inspections by Zip Code")
+    plt.axhline(y=10, xmin=0, xmax=100, color='b', linestyle='--')
+    plt.axhline(y=20, xmin=0, xmax=100, color='b', linestyle='--')
+    plt.axhline(y=30, xmin=0, xmax=100, color='b', linestyle='--')
+    plt.axhline(y=40, xmin=0, xmax=100, color='b', linestyle='--')
+    plt.axhline(y=50, xmin=0, xmax=100, color='b', linestyle='--')
+    plt.axhline(y=60, xmin=0, xmax=100, color='b', linestyle='--')
+    plt.axhline(y=70, xmin=0, xmax=100, color='b', linestyle='--')
+    plt.axhline(y=80, xmin=0, xmax=100, color='b', linestyle='--')
+    plt.show()
 
-plt.bar(x=filtered_df['premise_zip'], height=filtered_df['score'], color=["red"])
-plt.ylabel("Number of Failed Inspections")
-plt.xlabel(f"Zip Code \n {zipcode_count} \n  Zip Codes Found")
-plt.title("Inspections by Zip Code")
-plt.axhline(y=10, xmin=0, xmax=100, color='b', linestyle='--')
-plt.axhline(y=20, xmin=0, xmax=100, color='b', linestyle='--')
-plt.axhline(y=30, xmin=0, xmax=100, color='b', linestyle='--')
-plt.axhline(y=40, xmin=0, xmax=100, color='b', linestyle='--')
-plt.axhline(y=50, xmin=0, xmax=100, color='b', linestyle='--')
-plt.axhline(y=60, xmin=0, xmax=100, color='b', linestyle='--')
-plt.axhline(y=70, xmin=0, xmax=100, color='b', linestyle='--')
-plt.axhline(y=80, xmin=0, xmax=100, color='b', linestyle='--')
-plt.show()
+# Display widgets
+display(textbox, output)
